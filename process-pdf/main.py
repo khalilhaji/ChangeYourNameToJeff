c98e5bff8855ade7d12cbc11e22c3a0002dd59a0
Khalil Haji <khalilalihaji@gmail.com>
2018-10-13 13:58:05 -0400

    Changed return text for process-pdf

--- process-pdf/main.py
+++ process-pdf/main.py
@@ -7,4 +7,4 @@ storage_client = storage.Client()
 def process_pdfs(request):
     storage_client.get_bucket('cyn-templates').blob('california.pdf').download_to_filename('/tmp/test.pdf')
     storage_client.get_bucket('cyn-outputs').blob('test.pdf').upload_from_filename('/tmp/test.pdf')
-    return 'Done!'
+    return 'PDF ready to download!'
