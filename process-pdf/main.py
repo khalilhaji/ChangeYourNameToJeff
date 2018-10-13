f5578afb5bebb49324db42ce1389bfff2c30cfbc
Khalil Haji <khalilalihaji@gmail.com>
2018-10-13 13:45:03 -0400

    Added first cloud function

---
+++ process-pdf/main.py
@@ -0,0 +1,10 @@
+from google.cloud import storage
+
+storage_client = storage.Client()
+
+
+
+def process_pdfs(request):
+    storage_client.get_bucket('cyn-templates').blob('california.pdf').download_to_filename('/tmp/test.pdf')
+    storage_client.get_bucket('cyn-outputs').blob('test.pdf').upload_from_filename('/tmp/test.pdf')
+    return 'Done!'
